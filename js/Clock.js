1f8715c289f7d3f2690a2b39dd128fe6c5c42edc
sergBull <bullsergv@gmail.com>
2018-01-07 01:21:59 +0300

    Update Clock.js

--- js/Clock.js
+++ js/Clock.js
@@ -1,3 +1,5 @@
 document.body.onload = function() {
 	document.getElementById("Clock").innerHTML = new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds();
 }
+
+setTimeout(document.body.onload(), 1000);
